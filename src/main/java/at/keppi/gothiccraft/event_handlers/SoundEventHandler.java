e4de4cf9cfd2d44a0b04c380128945fb4bdfd2ce
PhilippKepplinger <philipp.kepplinger@gmx.at>
2022-01-16 13:32:53 +0100

    Add player death sound

--- src/main/java/at/keppi/gothiccraft/event_handlers/SoundEventHandler.java
+++ src/main/java/at/keppi/gothiccraft/event_handlers/SoundEventHandler.java
@@ -2,8 +2,10 @@ package at.keppi.gothiccraft.event_handlers;
 
 import at.keppi.gothiccraft.GothicCraft;
 import at.keppi.gothiccraft.lists.SoundRegistry;
+import at.keppi.gothiccraft.services.SoundService;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -16,5 +18,12 @@ public class SoundEventHandler {
     public static void onWakeup(PlayerWakeUpEvent event) {
         Player player = event.getPlayer();
         player.playSound(SoundRegistry.PLAYER_DEATH.get(), 1F, 1F);
+    }
+
+    @SubscribeEvent
+    public static void onLivingDeath(LivingDeathEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            SoundService.play(SoundRegistry.PLAYER_DEATH, player);
+        }
     }
 }
